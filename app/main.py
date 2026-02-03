d83f8eb51127a25ecb75fafca3eeede1baed9796
IZ <ivanzhelev@protonmail.com>
2026-02-03 16:13:53 +0200

    basic templating

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="app/frontend/pages")
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def home(request: Request):
+    ctx = {}
+    return templates.TemplateResponse(request, "base.html", context=ctx)
